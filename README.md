38098dfb14b505e1d7818c41dcc8a206ea0fe798
David Hassell <david.hassell@ncas.ac.uk>
2022-04-29 13:32:00 +0100

    dev

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # active-storage-1
 Demonstration of possible active storage approach
 
-Code changes to dask: https://github.com/davidhassell/dask/pull/1/files
+The demonstration in this repo uses a modified version of dask.
+
+Make sure to install this modified version of dask and include it in
+your python path, for instance (ymmv)
+
 
 ```bash
-$ 
+$ export PYTHONPATH=:$PYTHONPATH
+```
+
+Code changes in the modified dask are describes at
+https://github.com/davidhassell/dask/pull/1/files
+
+`cfdm` is also required to handle the reading of the netCDF file:
+
+```bash
+$ pip install cfdm
+```
+
+Code changes in the modified dask:
+https://github.com/davidhassell/dask/pull/1/files
+
+To run the demo:
+
+```bash
 $ cd active-storage-1
 $ python active_storage_demo.py
 
@@ -18,15 +39,15 @@ Non-active sum(a) = 1.843
     Normal sum(a) = 1.843
 
 Active max(a) + a = [[0.153 0.18  0.149 0.16  0.164 0.183 0.17  0.175]
- [0.169 0.182 0.191 0.208 0.192 0.219 0.152 0.212]
- [0.256 0.277 0.27  0.292 0.233 0.249 0.203 0.157]
- [0.175 0.205 0.185 0.216 0.204 0.218 0.155 0.163]
- [0.152 0.182 0.165 0.181 0.164 0.183 0.18  0.159]]
+                     [0.169 0.182 0.191 0.208 0.192 0.219 0.152 0.212]
+                     [0.256 0.277 0.27  0.292 0.233 0.249 0.203 0.157]
+                     [0.175 0.205 0.185 0.216 0.204 0.218 0.155 0.163]
+                     [0.152 0.182 0.165 0.181 0.164 0.183 0.18  0.159]]
 Normal max(a) + a = [[0.153 0.18  0.149 0.16  0.164 0.183 0.17  0.175]
- [0.169 0.182 0.191 0.208 0.192 0.219 0.152 0.212]
- [0.256 0.277 0.27  0.292 0.233 0.249 0.203 0.157]
- [0.175 0.205 0.185 0.216 0.204 0.218 0.155 0.163]
- [0.152 0.182 0.165 0.181 0.164 0.183 0.18  0.159]]
+                     [0.169 0.182 0.191 0.208 0.192 0.219 0.152 0.212]
+                     [0.256 0.277 0.27  0.292 0.233 0.249 0.203 0.157]
+                     [0.175 0.205 0.185 0.216 0.204 0.218 0.155 0.163]
+                     [0.152 0.182 0.165 0.181 0.164 0.183 0.18  0.159]]
 
 Active sum(max(a) + a) = 7.683
 Normal sum(max(a) + a) = 7.683
@@ -41,3 +62,5 @@ normal_max+a.png
 active_sum_max+a.png
 normal_sum_max+a.png
 ```
+
+The graph visualisations are also in the repo.
--- active_storage_demo.py
+++ active_storage_demo.py
@@ -11,19 +11,14 @@ class NetCDFArray:
 
     Supports active storage operations.
 
-    This object has been simplified from cfdm.NetCDFArray in order to
-    make the code easier to understand (e.g. it doesn't support netCDF
-    groups and doens't fully deal with string data types).
+    This object has been simplified from cfdm.NetCDFArray (o which it
+    is based) in order to make the code easier to understand. E.g. it
+    doesn't support netCDF groups and doesn't properly deal with
+    string data types.
 
     """
 
-    def __init__(
-        self,
-        filename=None,
-        ncvar=None,
-        dtype=None,
-        shape=None,
-    ):
+    def __init__(self, filename=None, ncvar=None, dtype=None, shape=None):
         self.filename = filename
         self.ncvar = ncvar
         self.dtype = dtype
@@ -37,10 +32,9 @@ class NetCDFArray:
         except RuntimeError as error:
             raise RuntimeError(f"{error}: {self.filename}")
 
-        
         array = netcdf.variables[self.ncvar][indices]
         # This where active storage would really happen
-        
+
         netcdf.close()
 
         # Spoof active storage
@@ -53,7 +47,7 @@ class NetCDFArray:
         if self.active_storage_op == "mean":
             array = (
                 np.full((1,) * array.ndim, array.size),
-                np.sum(array, axis=self.op_axis, keepdims=True)
+                np.sum(array, axis=self.op_axis, keepdims=True),
             )
 
         return array
@@ -65,12 +59,12 @@ class NetCDFArray:
         return f"file={self.filename} {self.ncvar}"
 
     def _active_chunk_functions(self):
-       return {
-           "min": self.active_min,
-           "max": self.active_max,
-           "mean": self.active_mean,
-       }
-   
+        return {
+            "min": self.active_min,
+            "max": self.active_max,
+            "mean": self.active_mean,
+        }
+
     @property
     def active_storage_op(self):
         return getattr(self, "_active_storage_op", None)
@@ -90,19 +84,19 @@ class NetCDFArray:
     @staticmethod
     def active_min(a, **kwargs):
         return a
-    
+
     @staticmethod
     def active_max(a, **kwargs):
         return a
-    
+
     @staticmethod
     def active_mean(a, **kwargs):
         return {"n": a[0], "total": a[1]}
-    
+
     def set_active_storage_op(self, op, axis=None):
         if op not in self._active_chunk_functions():
             raise ValueError(f"Invalid active storage operation: {op!r}")
-        
+
         a = self.copy()
         a.active_storage_op = op
         a.op_axis = axis
@@ -117,54 +111,64 @@ class NetCDFArray:
     def copy(self):
         return deepcopy(self)
 
+
 if __name__ == "__main__":
+    import os
+
     import cfdm
-    
-    f = NetCDFArray(filename='file.nc', ncvar='q',
-                    shape=(5, 8), dtype=np.dtype(float))
+    import dask
+
+    # Check that we're using the modified dask
+    try:
+        dask.array.reductions.actify
+    except AttributeError:
+        raise AttributeError(
+            "No 'dask.array.reductions.actify' function.\n"
+            f"dask path: {dask.__file__}\n"
+            f"PYTHONPATH={os.environ.get('PYTHONPATH', '')}"
+        )
+
+    f = NetCDFArray(filename="file.nc", ncvar="q", shape=(5, 8), dtype=np.dtype(float))
 
     # In-memory version of 'f'
-    x = cfdm.read('file.nc')[0].data.array
-    
+    x = cfdm.read("file.nc")[0].data.array
+
     df = da.from_array(f, chunks=(3, 4))
     dx = da.from_array(x, chunks=df.chunks)
-    
-    g = da.min(df)
-    y = da.min(dx)
-    print('\nActive max(a) =', g.compute())
-    print('Normal max(a) =', y.compute())
+
+    g = da.max(df)
+    y = da.max(dx)
+    print("\nActive max(a) =", g.compute())
+    print("Normal max(a) =", y.compute())
     g.visualize(filename="active_max.png")
     y.visualize(filename="normal_max.png")
-    
+
     g = da.mean(df)
     y = da.mean(dx)
-    print('\nActive mean(a) =', g.compute())
-    print('Normal mean(a) =', y.compute())
-    g.visualize(filename="active_max.png")
-    y.visualize(filename="normal_max.png")
-    
-    # da.sum has been "actified", but NetCDFArray does not support "sum"
-    # as an active operation
+    print("\nActive mean(a) =", g.compute())
+    print("Normal mean(a) =", y.compute())
+    g.visualize(filename="active_mean.png")
+    y.visualize(filename="normal_mean.png")
+
+    # da.sum has been "actified", but NetCDFArray does not support
+    # "sum" as an active operation
     g = da.sum(df)
     y = da.sum(dx)
-    print('\nNon-active sum(a) =', g.compute())
-    print('    Normal sum(a) =', y.compute())
+    print("\nNon-active sum(a) =", g.compute())
+    print("    Normal sum(a) =", y.compute())
     g.visualize(filename="non_active_sum.png")
     y.visualize(filename="normal_sum.png")
 
     g = da.max(df) + df
     y = da.max(dx) + dx
-    print('\nActive max(a) + a =', g.compute())
-    print('Normal max(a) + a =', y.compute())
+    print("\nActive max(a) + a =", g.compute())
+    print("Normal max(a) + a =", y.compute())
     g.visualize(filename="active_max+a.png")
     y.visualize(filename="normal_max+a.png")
-    
+
     g = da.sum(da.max(df) + df)
     y = da.sum(da.max(dx) + dx)
-    print('\nActive sum(max(a) + a) =', g.compute())
-    print('Normal sum(max(a) + a) =', y.compute())
+    print("\nActive sum(max(a) + a) =", g.compute())
+    print("Normal sum(max(a) + a) =", y.compute())
     g.visualize(filename="active_sum_max+a.png")
     y.visualize(filename="normal_sum_max+a.png")
-    
-
-
